aab6ee8358f90a22346a09cd1bf2c213975ea087
Minh Pham <minhpham@Minhs-MacBook-Pro-2.local>
2020-05-07 16:42:55 +0700

    Add

--- stocks/serializers.py
+++ stocks/serializers.py
@@ -111,7 +111,7 @@ class LastestFinancialReportsSerializer(serializers.ModelSerializer):
                     Q(Stock_id=stock_id)
                     & Q(Type=type)
                     & Q(ID=obj.ID) 
-                    (
+                    & (
                         (Q(Year=2020) & Q(Quarter=1))
                         | (Q(Year=2019) & Q(Quarter=4))
                         | (Q(Year=2019) & Q(Quarter=3))
